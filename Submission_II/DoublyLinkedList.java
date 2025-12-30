b38cbaa5299325e313233a443758f738e95d9702
Binupa_Ridmika <binuparidmika0821@gmail.com>
2025-12-31 00:11:12 +0530

    Implement Doubly Linked List with insert and delete

---
+++ Submission_II/DoublyLinkedList.java
@@ -0,0 +1,92 @@
+public class DoublyLinkedList {
+
+  
+    class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.prev = null;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+   
+    public void insert(int data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+        newNode.prev = temp;
+    }
+
+   
+    public void delete(int data) {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (head.data == data) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+            return;
+        }
+
+        Node temp = head;
+        while (temp != null && temp.data != data) {
+            temp = temp.next;
+        }
+
+        if (temp == null) {
+            System.out.println("Element not found");
+            return;
+        }
+
+        if (temp.next != null) {
+            temp.next.prev = temp.prev;
+        }
+        if (temp.prev != null) {
+            temp.prev.next = temp.next;
+        }
+    }
+
+   
+    public void display() {
+        Node temp = head;
+        System.out.print("Doubly Linked List: ");
+        while (temp != null) {
+            System.out.print(temp.data + " <-> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        DoublyLinkedList list = new DoublyLinkedList();
+
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+
+        list.display();
+
+        list.delete(20);
+        list.display();
+    }
+}
