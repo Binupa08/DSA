33dc7333ad48e999a308d8d8ec2b34299fa864b5
Binupa_Ridmika <binuparidmika0821@gmail.com>
2026-01-06 19:16:31 +0530

    Implement LinkedList-based stack with push/pop methods

---
+++ Submission_III/LinkedListStack.java
@@ -0,0 +1,62 @@
+public class LinkedListStack {
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node top;
+
+   
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = top;
+        top = newNode;
+        System.out.println(data + " pushed to stack");
+    }
+
+   
+    public int pop() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int popped = top.data;
+        top = top.next;
+        return popped;
+    }
+
+    
+    public void display() {
+        if (top == null) {
+            System.out.println("Stack is empty");
+            return;
+        }
+
+        System.out.print("Stack elements: ");
+        Node temp = top;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedListStack stack = new LinkedListStack();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        stack.display();
+
+        System.out.println("Popped: " + stack.pop());
+        stack.display();
+    }
+}
